a544545701571672e9411f81efc95a24c98b35b4
realvineeths <vineeth14shivu@gmail.com>
2023-08-15 23:40:03 +0530

    pre cramer

--- useBLE.js
+++ useBLE.js
@@ -8,6 +8,83 @@ import DeviceInfo from 'react-native-device-info';
 const bleManager = new BleManager();
 let beaconData = [];
 
+function determinantOfMatrix(mat) {
+  const ans=0;
+  ans = mat[0][0] * (mat[1][1] * mat[2][2] - mat[2][1] * mat[1][2])
+          - mat[0][1] * (mat[1][0] * mat[2][2] - mat[1][2] * mat[2][0])
+          + mat[0][2] * (mat[1][0] * mat[2][1] - mat[1][1] * mat[2][0]);
+  return ans;
+}
+
+function findSolution( coeff) {
+  // Matrix d using coeff as given in cramer's rule
+  var d = [
+          [coeff[0][0], coeff[0][1], coeff[0][2]],
+          [coeff[1][0], coeff[1][1], coeff[1][2]],
+          [coeff[2][0], coeff[2][1], coeff[2][2]],
+  ];
+
+  // Matrix d1 using coeff as given in cramer's rule
+  var d1 = [
+          [coeff[0][3], coeff[0][1], coeff[0][2]],
+          [coeff[1][3], coeff[1][1], coeff[1][2]],
+          [coeff[2][3], coeff[2][1], coeff[2][2]],
+  ];
+
+  // Matrix d2 using coeff as given in cramer's rule
+  var d2 = [
+          [coeff[0][0], coeff[0][3], coeff[0][2]],
+          [coeff[1][0], coeff[1][3], coeff[1][2]],
+          [coeff[2][0], coeff[2][3], coeff[2][2]],
+  ];
+
+  // Matrix d3 using coeff as given in cramer's rule
+  var d3 = [
+          [coeff[0][0], coeff[0][1], coeff[0][3]],
+          [coeff[1][0], coeff[1][1], coeff[1][3]],
+          [coeff[2][0], coeff[2][1], coeff[2][3]],
+  ];
+
+  // Calculating Determinant of Matrices d, d1, d2, d3
+  var D = determinantOfMatrix(d);
+  var D1 = determinantOfMatrix(d1);
+  var D2 = determinantOfMatrix(d2);
+  var D3 = determinantOfMatrix(d3);
+  System.out.printf("D is : %.6f \n", D);
+  System.out.printf("D1 is : %.6f \n", D1);
+  System.out.printf("D2 is : %.6f \n", D2);
+  System.out.printf("D3 is : %.6f \n", D3);
+
+  // Case 1
+  if (D != 0) {
+      // Coeff have a unique solution. Apply Cramer's Rule
+      var x = D1 / D;
+      var y = D2 / D;
+      var z = D3 / D; // calculating z using cramer's rule
+
+      System.out.printf("Value of x is : %.6f\n", x);
+      System.out.printf("Value of y is : %.6f\n", y);
+      System.out.printf("Value of z is : %.6f\n", z);
+
+      answer.setText(String.format("\nX: %.2f", x));
+      answer.append(String.format("\nY: %.2f", y));
+      answer.append("\nZ: 0");
+  }
+
+  // Case 2
+  else {
+      if (D1 == 0 && D2 == 0 && D3 == 0)
+          System.out.printf("Infinite solutions\n");
+      else if (D1 != 0 || D2 != 0 || D3 != 0)
+          System.out.printf("No solutions\n");
+
+      answer.setText("Location not found!");
+  }
+}
+
+
+
+
 
 function useBLE() {
   const [distance, setDistance] = useState([]);
@@ -31,6 +108,11 @@ function useBLE() {
     }
   };
 
+  function calculateDistance(rssi){
+    const currentDistance = Math.pow(10, (-55 - rssi) / (10 * 3));
+    return currentDistance;
+  }
+
   const scanForPeripherals = () =>
     bleManager.startDeviceScan(
       null,
@@ -41,12 +123,12 @@ function useBLE() {
       (error, device) => {
         if (device && device.name && device.name.includes('FSC')) {
           // console.log(device);
-          const currentDistance = Math.pow(10, (-55 - device.rssi) / (10 * 3));
+          
 
           const beacon = {
             id: device.id,
             rssi: device.rssi,
-            distance: currentDistance,
+            distance: calculateDistance(device.rssi)
           };
 
           // console.log(beacon);
@@ -63,8 +145,14 @@ function useBLE() {
 
           // console.log(beacon);
           for(let x=0;x<beaconData.length;x++){
-            console.log(beaconData[x].id);
+            console.log(beaconData[x]);
           }
+
+          // var r1 = new double[3][];
+          // r1[0] = new double[]{2.0d, 2.0d, 1.0d, ((Double) arrayList.get(0)).doubleValue()};
+          // r1[1] = new double[]{-2.0d, 2.0d, 1.0d, ((Double) arrayList.get(1)).doubleValue()};
+          // r1[2] = new double[]{2.0d, -2.0d, 1.0d, ((Double) arrayList.get(2)).doubleValue()};
+          // new GFG().findSolution(r1);
 
           setDistance([...beaconData]);
         }
@@ -73,6 +161,8 @@ function useBLE() {
         }
       },
     );
+
+
 
   return {
     scanForPeripherals,
